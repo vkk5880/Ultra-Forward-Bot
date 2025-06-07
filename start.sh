178701631e55dff0983a8f78f7ef93803cb2f80b
vkk5880 <41853419+vkk5880@users.noreply.github.com>
2025-06-07 10:05:48 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
-echo "Cloning Repo...."
+echo "Installing dependencies..."
+pip install --upgrade pip
+pip install -r /Ultra-Forward-Bot/requirements.txt
 
-if [ -z "$BRANCH" ]; then
-  echo "No BRANCH specified, using 'main'..."
-  git clone https://github.com/AkRao47/AK-T-Ultra-Forward-Bot /Ultra-Forward-Bot
-else
-  echo "Cloning branch: $BRANCH"
-  git clone -b "$BRANCH" https://github.com/AkRao47/AK-T-Ultra-Forward-Bot /Ultra-Forward-Bot
-fi
-
+echo "Starting Bot..."
 cd /Ultra-Forward-Bot || exit
-pip install -U -r requirements.txt
-
-echo "Starting Bot...."
 python3 main.py
+
+#!/bin/bash
+
+#echo "Cloning Repo...."
+
+#if [ -z "$BRANCH" ]; then
+ # echo "No BRANCH specified, using 'main'..."
+  #git clone https://github.com/AkRao47/AK-T-Ultra-Forward-Bot /Ultra-Forward-Bot
+#else
+  #echo "Cloning branch: $BRANCH"
+  #git clone -b "$BRANCH" https://github.com/AkRao47/AK-T-Ultra-Forward-Bot /Ultra-Forward-Bot
+#fi
+
+#cd /Ultra-Forward-Bot || exit
+#pip install -U -r requirements.txt
+
+#echo "Starting Bot...."
+#python3 main.py
